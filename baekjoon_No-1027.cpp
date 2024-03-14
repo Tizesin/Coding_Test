07dd668f336265c6c335e632a65154cece368be7
신민준 <161104180+Tizesin@users.noreply.github.com>
2024-03-14 23:19:43 +0900

    백준_알고리즘 1027번 문제풀이
    고층빌딩 문제 풀이
    선분 공식 사용

---
+++ baekjoon_No-1027.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int building_cnt;
+    int result = 0;
+    std::cin >> building_cnt;
+    int budilding_list[building_cnt][2] = {0,}; // 빌딩번호[x값] , 빌딩의 높이 , 보이는 건물
+
+    for(int i = 0; i < building_cnt; i++){
+        std::cin >> budilding_list[i][0];
+    }
+
+    for(int i = 0; i < building_cnt; i++){
+        int x1 = i;
+        int y1 = budilding_list[i][0];
+        for(int j = i+1; j<building_cnt; j++){
+            int x2 = j;
+            int y2 = budilding_list[j][0];
+            //선분공식 -> y = ax + b
+            double a1 = (y1-y2); // [x1 4 | y1 6][x2 11 | y2 7] -1 / -7
+            double a2 = (x1-x2); // [x1 4 | y1 6][x2 11 | y2 7] -1 / -7
+            double a = a1/a2; //(y1-y2)/(x1-x2); // [x1 4 | y1 6][x2 11 | y2 7] -1 / -7
+            double b = y1 - a*x1;
+            if(j-i==1){
+                budilding_list[i][1] += 1;
+                budilding_list[j][1] += 1;
+            }else {
+                for(int k = i+1; k < j; k++){
+                    if(budilding_list[k][0] >= a*k+b){
+                        break;
+                    }else{
+                        if(k==j-1){
+                            budilding_list[i][1] += 1;
+                            budilding_list[j][1] += 1;
+                        }
+                    }
+                }  
+            }
+        }
+    }
+    for(int i = 0; i < building_cnt; i++){
+        if(result < budilding_list[i][1])
+            result = budilding_list[i][1];
+    }
+    std::cout << result << std::endl;
+    
+    return  0;
+
+}
