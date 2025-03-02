a2313ba0a5198dc9ad78c96415b02e5ebca9847a
신민준 <161104180+Tizesin@users.noreply.github.com>
2025-03-02 23:04:26 +0900

    Create programmers_직사각형별찍기.java

---
+++ java11/programmers_직사각형별찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String str = "*";
+        while(b-- > 0){
+            System.out.println(str.repeat(a));
+        }
+    }
+}
